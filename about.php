9155696c33d5a62e6616deb535962da177bd5e95
Jeo <j.cabrigas.writer@gmail.com>
2023-01-18 21:02:22 -0800

    Creates root pages

---
+++ about.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php include 'sections/html_head.php';?>
+</head>
+
+<body>
+
+
+    <!-- Body Inner -->
+    <div class="body-inner">
+
+        <!-- Header -->
+        <?php include 'sections/header.php';?>
+
+        <!-- Content -->
+        <?php include 'content/about_content.php';?>
+
+        <!-- Footer -->
+        <?php include 'sections/footer.php';?>
+        
+    </div>
+    <!-- end: Body Inner -->
+
+    <!-- Scroll top -->
+    <a id="scrollTop"><i class="icon-chevron-up"></i><i class="icon-chevron-up"></i></a>
+
+    <!--Plugins-->
+    <?php include 'sections/js_end_scripts.php';?>
+
+</body>
+
+</html>
--- sections/header.php
+++ sections/header.php
@@ -5,8 +5,7 @@
             <div id="logo">
                 <a href="index.php">
                     <span class="logo">
-						<span class="logo text-center" style="color: #0fb7ff">My Wire Guy</span>
-						<small>By Tech 2U</small>
+						<span class="logo text-center" style="color: #0fb7ff">Jamieden Cabrigas</span>
 					</span>
                  </a>
             </div>
@@ -39,43 +38,7 @@
 									<li><a href="custom_built.php">Custom Built Computers</a></li>
 								</ul>
 							</li>
-							<!-- About Us -->
-							<li><a href="about_us.php">About Us</a></li>
-							<!-- Locations -->
-							<li class="dropdown"><a href="https://www.tech2u.com/locations/">Locations</a>
-							    <ul class="dropdown-menu">
-								    <li class="dropdown-submenu" style="padding: 0; min-width: 100px;"><span class="dropdown-menu-title-only">Northern California</span>
-									    <ul class="dropdown-menu" style="min-width: 165px">
-										    <li><a href="https://www.tech2u.com/sacramento/">Sacramento</a></li>
-											<li><a href="https://www.tech2u.com/rocklin-shop/">Rocklin</a></li>
-											<li><a href="https://www.tech2u.com/roseville/">Roseville</a></li>
-											<li><a href="https://www.tech2u.com/fair-oaks/">Fair Oaks</a></li>
-											<li><a href="https://www.tech2u.com/computer-repair-vallejo/">Vallejo</a></li>
-											<li><a href="https://www.tech2u.com/computer-repair-napa-valley/">Napa Valley</a></li>
-											<li><a href="https://www.tech2u.com/sacramento/computer-repairs-in-fairfield/">Fairfield</a></li>
-											<li><a href="https://www.tech2u.com/rancho-cordova/">Rancho Cordova</a></li>
-											<li><a href="https://www.tech2u.com/computer-repairs-in-stockton/">Stockton</a></li>
-											<li><a href="https://www.tech2u.com/computer-repairs-in-modesto/">Modesto</a></li>
-											<li><a href="https://www.tech2u.com/sacramento/computer-repair-in-vacaville/">Vacaville</a></li>
-											<li><a href="https://www.tech2u.com/computer-repair-tri-valley/">Tri Valley</a></li>
-										</ul>
-									</li>
-								    <li class="dropdown-submenu" style="padding: 0; min-width: 100px;"><span class="dropdown-menu-title-only">Southern California</span>
-									    <ul class="dropdown-menu" style="min-width: 185px">
-									        <li><a href="https://www.tech2u.com/carlsbad/">Carlsbad</a></li>
-									        <li><a href="https://www.tech2u.com/santee/">Santee</a></li>
-											<li><a href="https://www.tech2u.com/san-diego/">San Diego</a></li>
-											<li><a href="https://www.tech2u.com/hillcrest/">Hillcrest/Mission Hill</a></li>
-										</ul>
-								    </li>
-									<li class="dropdown-submenu" style="padding: 0; min-width: 100px;"><span class="dropdown-menu-title-only">Nevada</span>
-									    <ul class="dropdown-menu" style="min-width: 205px">
-										    <li><a href="https://www.tech2u.com/las-vegas/">Las Vegas - Henderson</a></li>
-											<li><a href="https://www.tech2u.com/las-vegas/">Las Vegas - Summerlin</a></li>
-										</ul>
-									</li>
-								</ul>
-							</li>
+							<li><a href="about.php">About</a></li>
 						</ul>
 						
 						<!-- Right Nav -->
@@ -83,7 +46,7 @@
 							<!-- FAQ -->
 							<li style="padding: 0;"><a href="faq.php">FAQ</a></li>
 							<!-- Contact -->
-							<li><a href="contact.php"> Contact Us</a></li>
+							<li><a href="contact.php"> Contact Me</a></li>
 							<!-- Phone -->
 							<li><a href="tel:8887352786"><i class="fa fa-phone"></i> 1-888-735-2786</a></li>
 						</ul>
