038bac11b0d50681af7691b0d5292dfe52900b4c
Jeo <j.cabrigas.writer@gmail.com>
2023-01-22 02:22:21 -0800

    Adds page headers to each page and leaves body open for page layouts

--- content/about_content.php
+++ content/about_content.php
@@ -1,23 +1,11 @@
-<section id="about-us-section">
+<section id="about-section">
 	<div class="container">
 		<div class="row">
 			<div class="col-12 page_header">
-				<h2>About Us</h2>
+				<h2>About</h2>
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-md-4 col-12 order-md-2">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12440.84787407499!2d-121.2413738!3d38.7817748!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa18ad5a7c3b6d4a1!2sTech%202U!5e0!3m2!1sen!2sus!4v1665072502708!5m2!1sen!2sus" width="400" height="320" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-			</div>
-			<div class="col-md-8 col-12 order-md-1">
-				<p>My Wire Guy opened in Rocklin, CA in 2009. Now, My Wire Guy is part of the <a href="https://www.tech2u.com/" target="_blank" id="about-us-tech2u" class="click_link">Tech 2U</a> computer repair family! Tech 2U and My Wire Guy offer Remote Support, In-Shop and On-site service to the entire greater Sacramento area. We offer computer repairs, new configurations and setups for home and businesses, no job is too big or small!<p>
-				
-				<p>We are located at the corner of Pacific Street and Sunset Blvd, next to Rocklin Florist.</p>
-				
-				<p>The store is open Monday - Friday from 9am - 6pm, closed from 1pm - 2pm for lunch.</p>
-				
-				<p>Remote support and on-site services are offered 7 days/week!  Give us a call today to book your appointment! <a href="tel:9166250141" class="click_link">(916) 625-0141</a></p>
-			</div>
 		</div>
 	</div>
 </section>
--- content/live_websites_content.php
+++ content/live_websites_content.php
@@ -2,35 +2,13 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-12 page_header">
-				<h2>We are an affordable, full service computer repair company.</h2>
+				<h2>Live Websites</h2>
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-lg-6 col-12">
-				<div class="row">
-					<div class="col-12">
-						<p>We offer a comprehensive set of computer related services performed by expert technicians to keep you and your computers operating most efficiently. Our services include:</p>
-						<div class="col-11 offset-1">
-							<ul class="list_links">
-								<li><a href ="computer_repair.php" class="click_link">Computer Repair & Optimization</a></li>
-								<li><a href ="networking.php" class="click_link">Networking</a></li>
-								<li><a href ="data_recovery.php" class="click_link">Data Recovery</a></li>
-								<li><a href ="custom_built.php" class="click_link">Custom Built Computers</a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-6 col-12">
-				<div class="row">
-					<div class="col-12 mobile_subheader">
-						<h4>We repair ALL brands of laptops and desktops -- Sony, Dell, Toshiba, HP, Gateway, Asus, and more.</h4>
-					</div>
-					<div class="col-12 contact_paragraph">
-						<p>For more information on any of our services, call us at <a href="tel:9166250141" class="click_link">(916) 625-0141</a>, email us, <a href="contact.php" class="click_link">contact us</a> through our website, or come into our Rocklin location Monday-Friday, 9:30 am to 6:00 pm-- <b>NO appointment needed!</b></p>
-					</div>
-				</div>
-			</div>
+			<div class="col-12">
+                
+            </div>
 		</div>
 	</div>
 </section>
