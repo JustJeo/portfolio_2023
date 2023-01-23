a1ad61afd455395d2416db4f309356f439b0b2ef
Jeo <j.cabrigas.writer@gmail.com>
2023-01-22 20:24:57 -0800

    Adds intro paragraph to Live Websites page and sets template for when content will be added

--- content/live_websites_content.php
+++ content/live_websites_content.php
@@ -7,8 +7,129 @@
 		</div>
 		<div class="row">
 			<div class="col-12">
+                <p>Here is a list of websites that I've worked on!</p>
                 
+                <p>Some of the websites are modern redesigns while others were built completely from scratch. If a client never had a prior website, I would create a wireframe that would then be submitted to the Senior Web Developer. Upon the approval of the Senior Web Developer, they would reach out to the client for the final approval. Once everything was given the green light, the building process of the website would begin.</p>
+                
+                <p>Each website is live and readily available to be viewed by the general public.</p>
             </div>
 		</div>
+
+        <!-- Laptops Plus -->
+        <div class="row">
+            <div class="col-12">
+                <h3>Laptops Plus</h3>
+            </div>
+            <div class="col-lg-6 col-12">
+                <img src="assets/images/" alt="Landing page preview for Laptops Plus">
+            </div>
+            <div class="col-lg-6 col-12">
+                
+            </div>
+        </div>
+
+        <!-- Solano Computer Guy -->
+        <div class="row">
+            <div class="col-12">
+                <h3>Solano Computer Guy</h3>
+            </div>
+            <div class="col-lg-6 col-12">
+                <img src="assets/images/" alt="Landing page preview for Solano Computer Guy">
+            </div>
+            <div class="col-lg-6 col-12">
+                
+            </div>
+        </div>
+
+        <!-- My Wire Guy -->
+        <div class="row">
+            <div class="col-12">
+                <h3>My Wire Guy</h3>
+            </div>
+            <div class="col-lg-6 col-12">
+                <img src="assets/images/" alt="Landing page preview for My Wire Guy">
+            </div>
+            <div class="col-lg-6 col-12">
+                
+            </div>
+        </div>
+
+        <!-- Bullard's Bar -->
+        <div class="row">
+            <div class="col-12">
+                <h3>Bullard's Bar</h3>
+            </div>
+            <div class="col-lg-6 col-12">
+
+            </div>
+            <div class="col-lg-6 col-12">
+                <img src="assets/images/" alt="Landing page preview for Bullard's Bar">
+            </div>
+        </div>
+
+        <!-- Bice -->
+        <div class="row">
+            <div class="col-12">
+                <h3>Bice Mobile Marine Boat Repair</h3>
+            </div>
+            <div class="col-lg-6 col-12">
+                <img src="assets/images/" alt="Landing page preview for Bice Mobile Marine Boat Repair">
+            </div>
+            <div class="col-lg-6 col-12">
+                
+            </div>
+        </div>
+
+        <!-- Flight -->
+        <div class="row">
+            <div class="col-12">
+                <h3>Stealth Accelerated Flight School</h3>
+            </div>
+            <div class="col-lg-6 col-12">
+
+            </div>
+            <div class="col-lg-6 col-12">
+                <img src="assets/images/" alt="Landing page preview for Stealth Accelerated Flight School">
+            </div>
+        </div>
+
+        <!-- Madden Lawyer -->
+        <div class="row">
+            <div class="col-12">
+                <h3>Madden Lawyer</h3>
+            </div>
+            <div class="col-lg-6 col-12">
+                <img src="assets/images/" alt="Landing page preview for Madden Lawyer">
+            </div>
+            <div class="col-lg-6 col-12">
+                
+            </div>
+        </div>
+
+        <!-- TLC Computer Solutions -->
+        <div class="row">
+            <div class="col-12">
+                <h3>TLC Computer Solutions</h3>
+            </div>
+            <div class="col-lg-6 col-12">
+
+            </div>
+            <div class="col-lg-6 col-12">
+                <img src="assets/images/" alt="Landing page preview for TLC Computer Solutions">
+            </div>
+        </div>
+
+        <!-- SD Computer Help -->
+        <div class="row">
+            <div class="col-12">
+                <h3>San Diego Computer Help</h3>
+            </div>
+            <div class="col-lg-6 col-12">
+                <img src="assets/images/" alt="Landing page preview for San Diego Computer Help">
+            </div>
+            <div class="col-lg-6 col-12">
+                
+            </div>
+        </div>
 	</div>
 </section>
