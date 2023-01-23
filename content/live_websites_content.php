c3c1044e049da59441b355c8eef904ff3d01b581
Jeo <j.cabrigas.writer@gmail.com>
2023-01-22 21:25:10 -0800

    Adds images and lorem ipsums to live websites

--- content/live_websites_content.php
+++ content/live_websites_content.php
@@ -15,6 +15,8 @@
             </div>
 		</div>
 
+        <hr>
+
         <!-- Laptops Plus -->
         <div class="row">
             <div class="col-12">
@@ -24,9 +26,11 @@
                 <img src="assets/images/laptops-plus.png" alt="Landing page preview for Laptops Plus">
             </div>
             <div class="col-lg-6 col-12">
-                
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisis sed odio morbi quis commodo odio aenean. Lacinia at quis risus sed vulputate odio ut enim blandit. Volutpat diam ut venenatis tellus in metus. Eu mi bibendum neque egestas congue quisque. Nunc mattis enim ut tellus elementum.</p>
             </div>
         </div>
+
+        <hr>
 
         <!-- Solano Computer Guy -->
         <div class="row">
@@ -34,12 +38,14 @@
                 <h3>Solano Computer Guy</h3>
             </div>
             <div class="col-lg-6 col-12">
-                <img src="assets/images/" alt="Landing page preview for Solano Computer Guy">
+                <p>Volutpat est velit egestas dui id ornare arcu odio ut. Enim facilisis gravida neque convallis a cras semper auctor. Ut porttitor leo a diam sollicitudin tempor. Sit amet venenatis urna cursus. Vitae semper quis lectus nulla at volutpat diam ut. Adipiscing commodo elit at imperdiet dui accumsan sit.</p>
             </div>
             <div class="col-lg-6 col-12">
-                
+                <img src="assets/images/solano-computer-guy.png" alt="Landing page preview for Solano Computer Guy">
             </div>
         </div>
+
+        <hr>
 
         <!-- My Wire Guy -->
         <div class="row">
@@ -47,12 +53,14 @@
                 <h3>My Wire Guy</h3>
             </div>
             <div class="col-lg-6 col-12">
-                <img src="assets/images/" alt="Landing page preview for My Wire Guy">
+                <img src="assets/images/my-wire-guy.png" alt="Landing page preview for My Wire Guy">
             </div>
             <div class="col-lg-6 col-12">
-                
+                <p>Ipsum suspendisse ultrices gravida dictum fusce ut placerat. Molestie nunc non blandit massa enim. Vitae ultricies leo integer malesuada. Lectus quam id leo in vitae turpis massa. Vitae nunc sed velit dignissim.</p>
             </div>
         </div>
+
+        <hr>
 
         <!-- Bullard's Bar -->
         <div class="row">
@@ -60,12 +68,14 @@
                 <h3>Bullard's Bar</h3>
             </div>
             <div class="col-lg-6 col-12">
-
+                <p>Orci eu lobortis elementum nibh tellus molestie nunc non. Lectus quam id leo in vitae turpis massa sed. Neque volutpat ac tincidunt vitae semper quis. Tincidunt vitae semper quis lectus. Turpis massa sed elementum tempus egestas sed sed. In aliquam sem fringilla ut. Malesuada pellentesque elit eget gravida cum sociis.</p>
             </div>
             <div class="col-lg-6 col-12">
-                <img src="assets/images/" alt="Landing page preview for Bullard's Bar">
+                <img src="assets/images/bullards-bar.png" alt="Landing page preview for Bullard's Bar">
             </div>
         </div>
+
+        <hr>
 
         <!-- Sircles -->
         <div class="row">
@@ -73,25 +83,14 @@
                 <h3>Sircles</h3>
             </div>
             <div class="col-lg-6 col-12">
-
+                <img src="assets/images/sircles.png" alt="Landing page preview for Sircles">
             </div>
             <div class="col-lg-6 col-12">
-                <img src="assets/images/" alt="Landing page preview for Sircles">
+                <p>Tristique senectus et netus et malesuada fames. Convallis convallis tellus id interdum velit laoreet id. Sed cras ornare arcu dui. Nibh sed pulvinar proin gravida. Accumsan lacus vel facilisis volutpat est. Ac tortor vitae purus faucibus. Dignissim diam quis enim lobortis scelerisque fermentum. Tincidunt ornare massa eget egestas purus viverra accumsan.</p>
             </div>
         </div>
 
-        <!-- Bice -->
-        <div class="row">
-            <div class="col-12">
-                <h3>Bice Mobile Marine Boat Repair</h3>
-            </div>
-            <div class="col-lg-6 col-12">
-                <img src="assets/images/" alt="Landing page preview for Bice Mobile Marine Boat Repair">
-            </div>
-            <div class="col-lg-6 col-12">
-                
-            </div>
-        </div>
+        <hr>
 
         <!-- Flight -->
         <div class="row">
@@ -99,12 +98,14 @@
                 <h3>Stealth Accelerated Flight School</h3>
             </div>
             <div class="col-lg-6 col-12">
-
+                <p>Pretium aenean pharetra magna ac placerat. Massa tincidunt nunc pulvinar sapien et ligula ullamcorper. Id faucibus nisl tincidunt eget nullam non nisi. Amet massa vitae tortor condimentum lacinia quis. Mollis nunc sed id semper risus in. Non tellus orci ac auctor augue mauris augue. Nibh nisl condimentum id venenatis a condimentum vitae. Sed elementum tempus egestas sed sed risus pretium quam vulputate.</p>
             </div>
             <div class="col-lg-6 col-12">
-                <img src="assets/images/" alt="Landing page preview for Stealth Accelerated Flight School">
+                <img src="assets/images/stealth-accelerated.png" alt="Landing page preview for Stealth Accelerated Flight School">
             </div>
         </div>
+
+        <hr>
 
         <!-- Madden Lawyer -->
         <div class="row">
@@ -112,12 +113,14 @@
                 <h3>Madden Lawyer</h3>
             </div>
             <div class="col-lg-6 col-12">
-                <img src="assets/images/" alt="Landing page preview for Madden Lawyer">
+                <img src="assets/images/madden-lawyer.png" alt="Landing page preview for Madden Lawyer">
             </div>
             <div class="col-lg-6 col-12">
-                
+                <p>A diam maecenas sed enim ut sem viverra aliquet. At auctor urna nunc id cursus metus aliquam eleifend. Orci a scelerisque purus semper eget duis at tellus. Sagittis purus sit amet volutpat. Nisl tincidunt eget nullam non nisi est sit. At augue eget arcu dictum varius. Nulla at volutpat diam ut venenatis tellus in metus.</p>
             </div>
         </div>
+
+        <hr>
 
         <!-- TLC Computer Solutions -->
         <div class="row">
@@ -125,12 +128,14 @@
                 <h3>TLC Computer Solutions</h3>
             </div>
             <div class="col-lg-6 col-12">
-
+                <p>Commodo odio aenean sed adipiscing diam donec. At quis risus sed vulputate odio. Id porta nibh venenatis cras sed felis eget velit. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Morbi tristique senectus et netus et malesuada fames. Purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Amet cursus sit amet dictum.</p>
             </div>
             <div class="col-lg-6 col-12">
-                <img src="assets/images/" alt="Landing page preview for TLC Computer Solutions">
+                <img src="assets/images/tlc-computer.png" alt="Landing page preview for TLC Computer Solutions">
             </div>
         </div>
+
+        <hr>
 
         <!-- SD Computer Help -->
         <div class="row">
@@ -138,10 +143,10 @@
                 <h3>San Diego Computer Help</h3>
             </div>
             <div class="col-lg-6 col-12">
-                <img src="assets/images/" alt="Landing page preview for San Diego Computer Help">
+                <img src="assets/images/sd-computer-help.png" alt="Landing page preview for San Diego Computer Help">
             </div>
             <div class="col-lg-6 col-12">
-                
+                <p>Volutpat lacus laoreet non curabitur gravida arcu. Mi tempus imperdiet nulla malesuada pellentesque elit. Ipsum a arcu cursus vitae congue mauris rhoncus. Massa massa ultricies mi quis hendrerit. Convallis aenean et tortor at risus viverra adipiscing at. Eu sem integer vitae justo eget magna. Adipiscing elit ut aliquam purus sit amet. Cras sed felis eget velit aliquet sagittis id consectetur.</p>
             </div>
         </div>
 	</div>
