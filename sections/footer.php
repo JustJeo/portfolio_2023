e96bcabeeb98801a7ddeaef00bfd43ffd8487740
Jeo <j.cabrigas.writer@gmail.com>
2023-01-25 01:14:35 -0800

    Adds images to footer, positions background image, and styles all forms of contact in footer

--- sections/footer.php
+++ sections/footer.php
@@ -1,46 +1,47 @@
 <footer id="footer">
 	<div class="footer-content">
-	    <section class="p-b-15 p-t-15" id="contact-img">
-		<div class="bg-overlay"></div>
+	    <section class="p-b-15 p-t-15" id="footer-img">
 	        <div class="container">
-		        <div class="row">
-					<div class="col-12">
-						<h2 class="m-b-10">Just Jeo</h2>
+		        <div class="row align-items-end">
+					<div class="col-md-2">
+						<h2 class="m-t-10" id="footer-title">Contact</h2>
 					</div>
-				</div>
-				<div class="row">
-			        <div class="col-md-4 col-sm-5 col-12">
-						<h4 class="m-b-0">Github</h4>
+
+                    <!-- Github -->
+			        <div class="col-md-2 col-sm-5 col-12 offset-md-3">
 				        <div class="row">
 							<div class="col-lg-12">
 								<div id="github-footer">
-									<p><a href="https://github.com/JustJeo" target="_blank">GitHub</a></p>
+									<a href="https://github.com/JustJeo" target="_blank" class="link_click underline">
+                                        <img src="assets/images/github-logo.png" id="github-logo-footer" class="footer_logo">
+                                    </a>
 								</div>
 							</div>
 						</div>
+                        <h4 class="m-t-0">Github</h4>
 					</div>
-					<div class="col-md-4 col-sm-7 col-12">
-                        <h4 class="m-b-0">LinkedIn</h4>
+
+                    <!-- LinkedIn -->
+					<div class="col-md-2 col-sm-7 col-12">
 				        <div class="row">
 							<div class="col-lg-12">
 								<div id="linkedin-footer">
-									<p><a href="https://www.linkedin.com/in/j-cabrigas/" target="_blank">LinkedIn</a></p>
+									<a href="https://www.linkedin.com/in/j-cabrigas/" target="_blank" class="link_click underline">
+                                        <img src="assets/images/linkedin-logo-black.png" id="linkedin-logo-footer" class="footer_logo">
+                                    </a>
 								</div>
 							</div>
 						</div>
+                        <h4 class="m-t-0">LinkedIn</h4>
 					</div>
-					<div class="col-md-4 col-sm-10 col-12" id="contact-footer">
-						<h4 class="m-b-0"></h4>
-						<div class="row">
-							<div class="col-12">
-								<div class="row">
-									<div class="col-12 footer_link">
-										<h5><span class="icon_spacing"><i class="icon-mail"></i></span><a href="mailto:j.cabrigas.writer@gmail.com" class="link_click underline">j.cabrigas.writer@gmail.com</a></h5>
-									</div>
-								</div>
-					        </div>
-				        </div>
+
+                    <!-- Email -->
+                    <div class="col-md-3 col-sm-10 col-12 footer_link" id="email-footer">
+                        <h5><a href="mailto:j.cabrigas.writer@gmail.com" class="link_click underline">j.cabrigas.writer@gmail.com</a></h5>
+
+                        <h4><span class="icon_spacing"><i class="icon-mail"></i></span>Email</h4>
 			        </div>
+                    
 		        </div>
 	        </div>
         </section>
