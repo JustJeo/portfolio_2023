a262658e52c22d2d3fd76c17ed411336814dd5bc
Jeo <j.cabrigas.writer@gmail.com>
2023-01-24 20:18:08 -0800

    Adds list of school projects to education page

--- content/technical_education_content.php
+++ content/technical_education_content.php
@@ -8,3 +8,87 @@
 		</div>
     </div>
 </section>
+
+
+
+<!-- School Projects -->
+<section id="school-projects-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+                <p>This list consists of projects I've made while I was enrolled in my coding bootcamp with General Assembly</p>
+            </div>
+		</div>
+
+        <hr>
+
+        <!-- Burger Builder -->
+        <div class="row">
+            <div class="col-lg-6 col-12">
+                <img src="assets/images/burger-builder.png" alt="Landing page preview for Burger Builder" class="project_image">
+            </div>
+            <div class="col-lg-6 col-12">
+                <h3 class="project_title link_click">Burger Builder</h3>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisis sed odio morbi quis commodo odio aenean. Lacinia at quis risus sed vulputate odio ut enim blandit. Volutpat diam ut venenatis tellus in metus. Eu mi bibendum neque egestas congue quisque. Nunc mattis enim ut tellus elementum.</p>
+            </div>
+        </div>
+
+        <hr>
+
+        <!-- Better Boothang -->
+        <div class="row">
+            <div class="col-lg-6 col-12">
+                <img src="assets/images/better-boothang.png" alt="Landing page preview for Better Boothang" class="project_image">
+            </div>
+            <div class="col-lg-6 col-12">
+                <h3 class="project_title link_click">Better Boothang</h3>
+                
+                <p>Volutpat est velit egestas dui id ornare arcu odio ut. Enim facilisis gravida neque convallis a cras semper auctor. Ut porttitor leo a diam sollicitudin tempor. Sit amet venenatis urna cursus. Vitae semper quis lectus nulla at volutpat diam ut. Adipiscing commodo elit at imperdiet dui accumsan sit.</p>
+            </div>
+        </div>
+
+        <hr>
+
+        <!-- Collabathon -->
+        <div class="row">
+            <div class="col-lg-6 col-12">
+                <img src="assets/images/collabathon.png" alt="Landing page preview for Collabathon" class="project_image">
+            </div>
+            <div class="col-lg-6 col-12">
+                <h3 class="project_title link_click">Collabathon</h3>
+
+                <p>Ipsum suspendisse ultrices gravida dictum fusce ut placerat. Molestie nunc non blandit massa enim. Vitae ultricies leo integer malesuada. Lectus quam id leo in vitae turpis massa. Vitae nunc sed velit dignissim.</p>
+            </div>
+        </div>
+
+        <hr>
+
+        <!-- Soul Sip'n -->
+        <div class="row">
+            <div class="col-lg-6 col-12">
+                <img src="assets/images/soul-sipn.png" alt="Landing page preview for Soul Sip'n" class="project_image">
+            </div>
+            <div class="col-lg-6 col-12">
+                <h3 class="project_title link_click">Soul Sip'n</h3>
+                
+                <p>Orci eu lobortis elementum nibh tellus molestie nunc non. Lectus quam id leo in vitae turpis massa sed. Neque volutpat ac tincidunt vitae semper quis. Tincidunt vitae semper quis lectus. Turpis massa sed elementum tempus egestas sed sed. In aliquam sem fringilla ut. Malesuada pellentesque elit eget gravida cum sociis.</p>
+            </div>
+        </div>
+
+        <hr>
+
+        <!-- Heart of Blink -->
+        <div class="row">
+            <div class="col-lg-6 col-12">
+                <img src="assets/images/heart-of-blink.png" alt="Landing page preview for Heart of Blink" class="project_image">
+            </div>
+            <div class="col-lg-6 col-12">
+                <h3 class="project_title link_click">Heart of Blink</h3>
+            
+                <p>Tristique senectus et netus et malesuada fames. Convallis convallis tellus id interdum velit laoreet id. Sed cras ornare arcu dui. Nibh sed pulvinar proin gravida. Accumsan lacus vel facilisis volutpat est. Ac tortor vitae purus faucibus. Dignissim diam quis enim lobortis scelerisque fermentum. Tincidunt ornare massa eget egestas purus viverra accumsan.</p>
+            </div>
+        </div>
+        
+	</div>
+</section>
