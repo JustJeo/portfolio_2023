84529ce120bbc3550130ec2f3eaa8b76826653c4
Jeo <j.cabrigas.writer@gmail.com>
2023-01-30 21:59:26 -0800

    Adds content into FAQ page

--- content/faq_content.php
+++ content/faq_content.php
@@ -65,23 +65,29 @@
 					<div class="ac-item">
 						<h5 class="ac-title">How did your tech journey start?</h5>
 						<div class="ac-content">
-							<p></p>
+							<p>I have to admit, the road to a technical career was very rocky for me in the beginning. Growing up in a strict Asian household, I was pressured to choose a career in the medical field. When I expressed to my parents that I wanted to work with computers, electronics, and major in Software Engineering, they believed that the only job I would land would be an electrician. They immediately told me that I could not take any classes regarding technology. No matter how many times I tried reasoning with them, it was always a stern "No" while my words went in one ear and out the other. As long as I was living under their roof, I had to abide by their rules. It had gotten to the point where, every semester, they would check my college classes and tell me to take off any classes that even sounded remotely technical.</p>
 							
-							<p></p>
+							<p>Let's fast forward to the middle of 2020, where I was unemployed and stuck indoors from the lockdown because of the pandemic. Times were tough, and I felt myself wondering what has my life become. If this was the end, would I be happy with how my life turned out? At that moment, I would have answered, "No" because I wasn't able to pursue the career that I've always wanted to do. So I figured that instead of just letting the days pass by, I could finally pursue my dreams.</p>
+
+                            <p>At this point, I had already moved out of my parents house and was free to remotely do the things that I've wanted to do. So I did research on how to get into the technical field and discovered things called "Coding Bootcamps". From there, the rest is history. If you want to know the full in-depth story from here, you can find it on my "Education" page. </p>
 						</div>
 					</div>
 					<div class="ac-item">
 						<h5 class="ac-title">What is your school and work history in the tech industry?</h5>
 						<div class="ac-content">
-							<p><p>
+							<p>I wasn't able to do much with technology while I was in high school and college, but once I moved out and was no longer under my parents' roof with their rules, I sent myself to a coding bootcamp to further pursue my love for technology. To read the whole story in-depth and with a complete timeline, simply click on the "Education" page on the left.<p>
 
-							<p></p>
+							<p>When I graduated from my coding bootcamp, the pandemic was still in full force and lockdown was still in place. It was hard for me to land a job in the tech industry as many companies were still trying to survive with the little amount of employees they had left. Still, I persisted and kept applying endlessly. Even when I felt like my imposter syndrome was right about everything, I still made sure to apply.</p>
+
+                            <p>Eventually, I was contacted by a local company in my area named Tech 2U. They had just started their web development team and needed a frontend developer to join on. After a phone interview, assessment test, and an in-person interview, I was offered the job. I have been working with Tech 2U since July 2021, first as a contractor, then as a part-timer in August 2021, and then officially as a full-time employee in October 2021. </p>
 						</div>
 					</div>
 					<div class="ac-item">
 						<h5 class="ac-title">Do you see yourself working with technology for the rest of your life?</h5>
 						<div class="ac-content">
-							<p></p>
+							<p>In all honesty, yes, I absolutely do. Growing up, I was always so fascinated with technology, no matter what it was. Whether it was the latest video game, the newest cell phone model, brand new cars with digital center consoles, to even things like fridges with screens attached to them! Things like that always fascinated and as I got older I started to think things like, "Wouldn't it be cool to build that?" or "I wonder how they programmed that."</p>
+
+                            <p>As the years would go by, newer, more complicated gizmos and gadgets would be released and I would always want to be a part of a team that created those technical fascinations. I've never had a passion for something as much as I do with technology. The lights, the programming, the futuristic design of it all, it mesmerizes me and it makes me crave the future more. I absolutely want to work with technology for the rest of my life. It makes me happy.</p>
 						</div>
 					</div>
 				</div>
@@ -100,7 +106,11 @@
 					<div class="ac-item">
 						<h5 class="ac-title">Why did you start streaming on Twitch?</h5>
 						<div class="ac-content">
-							<p>I love playing video games.</p>
+							<p>Over the years I realized that I started collecting games that I've always wanted to play, but never got around to actually playing. At one point, I wondered what I was going to do with all of these digital games. For those who may not know, digital games are locked to a single account and cannot be traded or exchanged like physical copies can. It was then that I remembered some of my favorite YouTubers saying things like, "I never got to play this before, so I'm finally able to now!" I then told myself I was going to record YouTube videos and have a gaming channel too. That way I could become a content creator while playing my favorite games - and if I were good enough, then I would could get paid to play games!... Aaaand then I realized how strict YouTube was with their videos. So then I looked for an alternate route.</p>
+
+                            <p>I heard a lot of people talking about live streaming their gaming sessions. They would stream live, save their streaming sessions, and then upload to YouTube if it were safe. I figured, since YouTube was so intimidating to me, I would just cut out the video portion of the plan and only stream live. Unless you were doing something absolutely inappropriate, you could play music and play games with almost no restrictions, as long as you didn't save the streaming session.</p>
+
+                            <p>I've been streaming ever since then. Not with a set schedule, but still with all the fun.</p>
 						</div>
 					</div>
 					<div class="ac-item">
