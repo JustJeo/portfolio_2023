232abf92375b194d692c270d59952bd12d92e12e
Jeo <j.cabrigas.writer@gmail.com>
2023-01-31 22:28:15 -0800

    Adds card to strengths and weaknesses section

--- content/about_content.php
+++ content/about_content.php
@@ -35,12 +35,16 @@
                     <div class="card-header text-center">
                         <h3>Strengths</h3>
                     </div>
-                    <div class="card-body bg-dark">
-                        <ul class="text-light">
-                            <li><span class="bold underline">Problem-Solver:</span> I am able to solve problems quickly and efficiently by looking at the situation from multiple perspectives and thinking of multiple answers.</li>
-                            <li><span class="bold underline">Detail-Oriented:</span> I pay close attention to detail and notice things that most people tend to miss, knowing that every little thing can mean a lot in the long run.</li>
-                            <li><span class="bold underline">Collaborative:</span> I am confident in my ability to clearly and effectively communicate with a team to keep everyone on the same page while striving to reach the final product.</li>
-                        </ul>
+                    <div class="card-body bg-secondary">
+                        <div class="row">
+                            <div class="col-12">
+                                <ul class="text-light">
+                                    <li><span class="bold underline">Problem-Solver:</span> I am able to solve problems quickly and efficiently by looking at the situation from multiple perspectives and thinking of multiple answers.</li>
+                                    <li><span class="bold underline">Detail-Oriented:</span> I pay close attention to detail and notice things that most people tend to miss, knowing that every little thing can mean a lot in the long run.</li>
+                                    <li><span class="bold underline">Collaborative:</span> I am confident in my ability to clearly and effectively communicate with a team to keep everyone on the same page while striving to reach the final product.</li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -50,11 +54,15 @@
                         <h3>Weaknesses</h3>
                     </div>
                     <div class="card-body bg-dark">
-                        <ul class="text-light">
-                            <li><span class="bold underline">Self-Criticism:</span> I tend to be overly critical of myself. Whenever I complete a project, I can't help but feel that I could have done more even if my work received a positive response.</li>
-                            <li><span class="bold underline">Too Introverted:</span> I am incredibly introverted, which makes me wary of sharing my ideas in a group setting or speaking up during team meetings. I feel that I had good intentions, I just wasn't always comfortable speaking up.</li>
-                            <li><span class="bold underline">Lack of Experience:</span> </li>
-                        </ul>
+                        <div class="row">
+                            <div class="col-12">
+                                <ul class="text-light">
+                                    <li><span class="bold underline">Self-Criticism:</span> I tend to be overly critical of myself. Whenever I complete a project, I can't help but feel that I could have done more even if my work received a positive response.</li>
+                                    <li><span class="bold underline">Too Introverted:</span> I am incredibly introverted, which makes me wary of sharing my ideas in a group setting or speaking up during team meetings. I feel that I had good intentions, I just wasn't always comfortable speaking up.</li>
+                                    <li><span class="bold underline">Lack of Experience:</span> </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
