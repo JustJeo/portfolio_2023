71e4726faaa336317e4438fe53a7700e7ae521e6
Jeo <j.cabrigas.writer@gmail.com>
2023-02-01 00:30:32 -0800

    Makes styling changes to about section cards and titles

--- content/about_content.php
+++ content/about_content.php
@@ -13,7 +13,7 @@
     <div class="container">
         <div class="card">
             <div class="card-body">
-                <div class="row">
+                <div class="row" id="about-intro">
                     <!-- Intro -->
                     <div class="col-12">
                         <h3>My name is <span class="blue_text">Jamieden Cabrigas</span>. I also go by Jamie!</h3>
@@ -26,21 +26,21 @@
                 </div>
 
                 <!-- Cards -->
-                <div class="row">
-                    <div class="col-lg-4">
+                <div class="row" id="strengths-and-weaknesses">
+                    <div class="col-lg-12">
                         <h3>Strengths & Weaknesses</h3>
 
                         <p>Something that's really hard for people to reveal is their strengths and weaknesses. They may find themselves being too boastful if they name their positives. On the other hand, they don't like to point out the negatives within themselves. To me, every strength can have their weakness, and every weakness can have their lesson to learn from. It's all just a matter of seeing the silver lining that shines through.</p>
                     </div>
-                    <div class="col-lg-4">
-                        <div class="card">
-                            <div class="card-header text-center">
+                    <div class="col-lg-6">
+                        <div class="card" id="strengths-card">
+                            <div class="card-header bg-success text-light text-center">
                                 <h3>Strengths</h3>
                             </div>
-                            <div class="card-body bg-secondary">
+                            <div class="card-body">
                                 <div class="row">
                                     <div class="col-12">
-                                        <ul class="text-light">
+                                        <ul>
                                             <li><span class="bold underline">Problem-Solver:</span> I am able to solve problems quickly and efficiently by looking at the situation from multiple perspectives and thinking of multiple answers.</li>
                                             <li><span class="bold underline">Detail-Oriented:</span> I pay close attention to detail and notice things that most people tend to miss, knowing that every little thing can mean a lot in the long run.</li>
                                             <li><span class="bold underline">Collaborative:</span> I am confident in my ability to clearly and effectively communicate with a team to keep everyone on the same page while striving to reach the final product.</li>
@@ -50,18 +50,18 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-4">
-                        <div class="card">
-                            <div class="card-header text-center">
+                    <div class="col-lg-6">
+                        <div class="card" id="weaknesses-card">
+                            <div class="card-header bg-dark text-light text-center">
                                 <h3>Weaknesses</h3>
                             </div>
-                            <div class="card-body bg-dark">
+                            <div class="card-body">
                                 <div class="row">
                                     <div class="col-12">
-                                        <ul class="text-light">
+                                        <ul>
                                             <li><span class="bold underline">Self-Criticism:</span> I tend to be overly critical of myself. Whenever I complete a project, I can't help but feel that I could have done more even if my work received a positive response.</li>
                                             <li><span class="bold underline">Too Introverted:</span> I am incredibly introverted, which makes me wary of sharing my ideas in a group setting or speaking up during team meetings. I feel that I had good intentions, I just wasn't always comfortable speaking up.</li>
-                                            <li><span class="bold underline">Lack of Experience:</span> </li>
+                                            <li><span class="bold underline">Lack of Experience:</span> I know that compared to others in the world of technology, I lack the experience and knowledge of languages and skills. However, I am confident with learning and adapting to new environments and skillsets.</li>
                                         </ul>
                                     </div>
                                 </div>
