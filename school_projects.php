4c19061356dc85ce9d5e753bf3077ad660d8a2bb
Jeo <j.cabrigas.writer@gmail.com>
2023-02-03 13:00:25 -0800

    Splits education and school projects into 2 pages

---
+++ school_projects.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <?php include 'sections/html_head.php';?>
+    </head>
+
+    <body class="side-panel side-panel-static">
+
+        <!-- Side Panel -->
+        <?php include 'sections/side_panel.php';?>
+
+        <!-- Body Inner -->
+        <div class="body-inner">
+
+            <!-- Content -->
+            <?php include 'content/school_projects_content.php';?>
+
+            <!-- Footer -->
+            <?php include 'sections/footer.php';?>
+
+        </div>
+        <!-- end: Body Inner -->
+
+        <!-- Scroll top -->
+        <a id="scrollTop"><i class="icon-chevron-up"></i><i class="icon-chevron-up"></i></a>
+
+        <!--Plugins-->
+        <?php include 'sections/js_end_scripts.php';?>
+
+    </body>
+
+</html>
