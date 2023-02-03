8cf6f8b3572584bec282416aeb7220efb18bb2b0
Jeo <j.cabrigas.writer@gmail.com>
2023-02-02 21:42:56 -0800

    Adds detailed content to website portal

--- ajax/laptops_plus_info.php
+++ ajax/laptops_plus_info.php
@@ -3,7 +3,11 @@
     <button type="button" class="btn btn-light btn-sm float-right" id="x-btn">X</button>
 </div>
 <div class="card-body">
-    <p>Not long after getting hired with Tech 2U, I was assigned the task of remaking one of the websites of a business they acquisitioned. This was a purely static and informational website that needed a more modern touch. Using Bootstrap, I was able to place rows, columns, and containers to set sections in place.</p>
+    <p>Not long after being hired at Tech 2U, the company asked me to redesign a website from another company that they had recently acquisitioned. They wanted my version of the website to look modern and welcoming while still displaying a lot of information to any visitors. Since this was going to be my very first website, the Senior Web Developer encouraged me to use this as a testing environment. He wanted me to familiarize myself with the company syntax, the Bootstrap libraries they use, and to try things I had never done before.</p>
+
+    <p>As this was my first time setting up a live dev environment, I had to get used to porting over libraries, creating directories, and connect paths to the correct file. I used PHP to include sections and content into root files while using AJAX to set up a phpMailer for the contact form. As I was still very new to all of this, the Senior Web Developer worked on the functionality of the mailer. In one of my school projects, we had used a carousel, so I wanted to refresh my memory on how to implement that feature by using it on the landing page. I made a lot of references to documentations and to the original Laptops Plus website. I wanted to keep a lot of the original color schemes so I kept the logo blue and red while keeping the powder blue colors for text boxes, hover effects, and clickable links. I was also able to test out video backgrounds for sections and placing opacity overlays for easier reading.</p>
+
+    </h5><span class="green_text bold underline">Technologies Used:</span> HTML, CSS, JavaScript, PHP, AJAX, Bootstrap</h5>
 
     <div class="row">
         <div class="col-12 text-center">
