39468e66eefe279cd0465a84845958eb63c4ccd5
Jeo <j.cabrigas.writer@gmail.com>
2023-02-04 01:20:37 -0800

    Finalizes footer

--- sections/footer.php
+++ sections/footer.php
@@ -2,51 +2,47 @@
 	<div class="footer-content">
 	    <section class="p-b-15 p-t-15" id="footer-img">
 	        <div class="container">
-		        <div class="row align-items-end">
-					<div class="col-md-2">
-						<h2 class="m-t-10" id="footer-title">Contact</h2>
-					</div>
-
-                    <!-- Github -->
-			        <div class="col-md-2 col-sm-5 col-12 offset-md-3" id="github-footer-col">
-				        <div class="row">
-							<div class="col-lg-12">
-								<div id="github-footer">
-									<a href="https://github.com/JustJeo" target="_blank" class="link_click underline">
+		        <div class="row align-items-center">
+					<div class="col-md-8">
+                        <div class="row">
+                            <div class="col-md-4 text-center footer_link" id="email-footer">
+						        <!-- Email -->
+                                <h4><a href="mailto:j.cabrigas.writer@gmail.com" class="link_click underline">j.cabrigas.writer@gmail.com</a></h4>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <!-- Github -->
+                            <div class="col-md-2 text-center" id="github-footer-col">
+                                <div id="github-footer">
+                                    <a href="https://github.com/JustJeo" target="_blank" class="link_click underline">
                                         <img src="assets/images/github-logo.png" id="github-logo-footer" class="footer_logo">
                                     </a>
-								</div>
-							</div>
-						</div>
-                        <h4 class="m-t-0">Github</h4>
-					</div>
+                                </div>
+                                <h4 class="m-t-0">Github</h4>
+                            </div>
 
-                    <!-- LinkedIn -->
-					<div class="col-md-2 col-sm-7 col-12">
-				        <div class="row">
-							<div class="col-lg-12">
-								<div id="linkedin-footer">
-									<a href="https://www.linkedin.com/in/j-cabrigas/" target="_blank" class="link_click underline">
+                            <!-- LinkedIn -->
+                            <div class="col-md-2 text-center">
+                                <div id="linkedin-footer">
+                                    <a href="https://www.linkedin.com/in/j-cabrigas/" target="_blank" class="link_click underline">
                                         <img src="assets/images/linkedin-logo-black.png" id="linkedin-logo-footer" class="footer_logo">
                                     </a>
-								</div>
-							</div>
-						</div>
-                        <h4 class="m-t-0">LinkedIn</h4>
+                                </div>
+                                <h4 class="m-t-0">LinkedIn</h4>
+                            </div>
+                        </div>
 					</div>
 
-                    <!-- Email -->
-                    <div class="col-md-3 col-sm-10 col-12 footer_link" id="email-footer">
-                        <h5><a href="mailto:j.cabrigas.writer@gmail.com" class="link_click underline">j.cabrigas.writer@gmail.com</a></h5>
-
-                        <h4><span class="icon_spacing"><i class="icon-mail"></i></span>Email</h4>
-			        </div>
-                    
+                    <div class="col-md-4">
+                        <div class="row">
+                            <div class="col-md-7 text-center">
+                                <!-- Copyright -->
+                                <div class="copyright-text">Copyright &copy; <?php echo date("Y");?> Jamieden Cabrigas.<br>Once again I'm Jeo, Just Jeo.</div>
+                            </div>
+                        </div>
+                    </div>
 		        </div>
 	        </div>
         </section>
-	</div>
-	<div class="copyright-content">
-		<div class="copyright-text text-center">Copyright &copy; <?php echo date("Y");?> Jamieden Cabrigas. Once again I'm Jeo, Just Jeo.</div>
 	</div>
 </footer>
