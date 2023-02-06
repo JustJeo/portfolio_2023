8e504b5bcaa97a793fdeba644410e520287a9c4b
Jeo <j.cabrigas.writer@gmail.com>
2023-02-05 21:45:11 -0800

    Fixes grammar and spelling mistakes on Education page

--- content/education_content.php
+++ content/education_content.php
@@ -18,7 +18,7 @@
                 <h3 class="underline">High School & College</h3>
             </div>
             <div class="col-12">
-                <p>During high school, I knew that I wanted to have a career in the technical field. I wanted to graduate and go into college with a major in Computer Science. However, growing up in a strict Asian household, I was pushed more towards a medical career than a technical one. While I still lived under my parents' roof, I was told that I couldn't take any computer classes because they belived that I would only end up as an electrician. By the time I graduated from college with an Associate's Degree in an Interdisciplinary Studies of Math and Science, I had only 2 computer classes under my belt.</p>
+                <p>During high school, I knew that I wanted to have a career in the technical field. I wanted to graduate and go into college with a major in Computer Science. However, growing up in a strict Asian household, I was pushed more toward a medical career than a technical one. While I still lived under my parents' roof, I was told that I couldn't take any computer classes because they believed that I would only end up as an electrician. By the time I graduated from college with an Associate's Degree in Interdisciplinary Studies of Math and Science, I had only 2 computer classes under my belt.</p>
             </div>
         </div>
         
@@ -28,7 +28,7 @@
                 <h3 class="underline">Coding Bootcamp</h3>
             </div>
             <div class="col-12">
-                <p>It wasn't until I moved out that I was able to send myself to school with a more technical goal in mind. My entire technical journey was completed without my parents' knowledge. Here is how I did it.</p>
+                <p>It wasn't until I moved out that I was able to send myself to school with a more technical goal in mind. My entire technical journey was completed without my parents' knowledge. Here is how I did it!</p>
             </div>
         </div>
         <div class="row">
@@ -40,13 +40,13 @@
                         <div class="timeline-icon">1</div>
                         <h4>Planning and Research</h4>
                         <div class="timeline-item-date">Early June 2020</div>
-                        <p>The pandemic was in full force during this time and I was unemployed and stuck indoors because of a state-wide lockdown. As I sat and watched the days go by, I knew that I wanted to do more with my life. I wanted to take charge, instead of just sitting around, waiting for something to happen.</p>
+                        <p>The pandemic was in full force during this time and I was unemployed and stuck indoors because of a state-wide lockdown. As I sat and watched the days go by, I knew that I wanted to do more with my life. I wanted to take charge instead of just sitting around, waiting for something to happen.</p>
 
                         <p>Eventually, I remembered the vision I had lost so long ago. I wanted a career in the technical field. Now living on my own with no one to stop me, I started doing research and planned out my future.</p>
 
                         <p>Everything was remote, obviously, and it was just a matter of finding out which coding bootcamp to settle with. I primarily looked for programs that offered a "Learn Now, Pay Later" type of tuition because I knew that I wouldn't be able to afford it right away.</p>
 
-                        <p>This is where I came across a coding bootcamp called "General Assembly". They provided everything I was looking for, and was ranked as one of the highest coding bootcamps available.</p>
+                        <p>This is where I came across a coding bootcamp called "General Assembly". They provided everything I was looking for and were ranked as one of the highest coding bootcamps available.</p>
                     </li>
 
                     <!-- Timeline Item -->
@@ -55,12 +55,12 @@
                         <h4>Course Application Interview</h4>
                         <div class="timeline-item-date">Late June 2020</div>
                         <p>After days of research and countless hours of debating with myself, I finally sent General Assembly an application to enroll in their Software Engineering Immersive program.</p>
-
-                        <p>Unlike many other coding bootcamps I came across, General Assembly wanted to do an over-the-phone interview with all applicants before allowing them to enroll in any program they offered. This was to ensure that all students who took their courses were actually serious in having a career in the technical field. It seemed to me like General Assembly actually cared about their students and really wanted them to succeed.</p>
-
-                        <p>My application agent's name was Brian. He told me that he was checking to see if I was truely interested in learning to become a software engineer or if I was just trying to find a hobby during the pandemic that would pass the time until the lockdown was lifted. He saw my passion and how genuine I was to have a career with technology. I had passed the application interview.</p>
-
-                        <p>Despite passing the interview, Brian warned me that this was just the beginning. If I were really serious about becoming a Software Engineer, then I would work hard to prove it. In about a month and a half, I would have to pass all of the pre-course work, pass an assessment test, and build a website about myself that I would then have to present to him. I could only use HTML, CSS, and Vanilla JavaScript for my website. Brian sent me some beginner material to help me get started, but urged for me to take a prep course if possible.</p>
+                        
+                        <p>Unlike many other coding bootcamps I came across, General Assembly wanted to do an over-the-phone interview with all applicants before allowing them to enroll in any program they offered. This was to ensure that all students who took their courses were serious about having a career in the technical field. It seemed to me like General Assembly cared about their students and really wanted them to succeed.</p>
+                        
+                        <p>My application agent's name was Brian. He told me that he was checking to see if I was truly interested in learning to become a software engineer or if I was just trying to find a hobby during the pandemic that would pass the time until the lockdown was lifted. He saw my passion and how genuine I was to have a career in technology. I passed the application interview!</p>
+                        
+                        <p>Despite passing the interview, Brian warned me that this was just the beginning. If I were really serious about becoming a software engineer, then I would have to work hard to prove it. In about a month and a half, I would be required to pass all of the pre-course work, pass an assessment test, and build a website about myself that I would then have to present to him. I could only use HTML, CSS, and Vanilla JavaScript for my website. Brian sent me some beginner material to help me get started but urged me to take a prep course if possible.</p>
                     </li>
 
                     <!-- Timeline Item -->
@@ -68,13 +68,13 @@
                         <div class="timeline-icon">3</div>
                         <h4>Springboard</h4>
                         <div class="timeline-item-date">Early July 2020 - Early August 2020</div>
-                        <p>Taking Brian's advice to get some exposure to HTML, CSS, and some JavaScript, I had taken an introductory class held by an online learning program named, Springboard. I remember seeing them as a competitor to General Assembly when I was doing my initial coding bootcamp research. Since General Assembly didn't offer a prep course, I figured to go with one of my other choices that did offer one. The course was called the "Software Engineering Career Track Prep Course".</p>
-
+                        <p>Taking my application agent's advice to get some exposure to HTML, CSS, and JavaScript, I took an introductory class held by an online learning program named Springboard. I remember seeing them as a competitor to General Assembly when I was doing my initial coding bootcamp research. Since General Assembly didn't offer a prep course, I figured to go with one of my other choices that did offer one. The course was called the "Software Engineering Career Track Prep Course".</p>
+                        
                         <p>Once a week, for six weeks, a mentor would meet with me to go over the homework and lessons. They were there for me to ask questions if I needed any help or had a hard time grasping a certain concept. They would also go over any questions that I had gotten wrong on the quiz at the end of every lesson.</p>
-
-                        <p>One thing I did not know after being enrolled, was that after completing the prep course, I would automatically be enrolled in their "Software Engineering Career Track". This was going to be a problem.</p>
-
-                        <p>After voicing my concern to my Springboard mentor about the auto-enrollment into their full-time course, we had said a bittersweet good-bye.</p>
+                        
+                        <p>One thing I did not know after being enrolled was that after completing the prep course, I would automatically be enrolled in their "Software Engineering Career Track". This was going to be a problem.</p>
+                        
+                        <p>After voicing my concerns to my Springboard mentor about the auto-enrollment into their full-time course, we said a bittersweet goodbye.</p>
                     </li>
 
                     <!-- Timeline Item -->
@@ -82,15 +82,15 @@
                         <div class="timeline-icon">4</div>
                         <h4>Assessment Test, Website Evaluation & Scholarship</h4>
                         <div class="timeline-item-date">August 12th, 2020</div>
-                        <p>I was now prepared to take the assessment test and build my very first portfolio. The only problem was that I had one week left, despite originally have six weeks to work on everything.</p>
-
-                        <p>Working hard to absorb every bit of HTML, CSS, and JavaScript that I could, I finished all of the pre-course work in a few nights. I then passed the assessment test with an almost perfect score the next day. In the end, I was left with three days to build my portfolio from scratch.</p>
-
-                        <p>Brian hopped into a Zoom meeting with me on August 12th, 2020 for my Website Evaluation. He was quiet for a majority of the presentation and let me explain my code. At the end, he asked if I had anything else to say about my website, and I confessed to him that I had only made it within the past three days.</p>
-
-                        <p>I had always kept Brian in the loop with my progress for the Springboard prep course. He didn't know it would take so much time away from me and he seemed concerned that all the material for General Assembly was done in one week.</p>
+                        <p>I was now prepared to take the assessment test and build my very first portfolio. The only problem was that I had one week left, despite originally having six weeks to work on everything.</p>
                         
-                        <p>Despite my struggles, he told me that he was genuinely impressed with my portfolio website. He confessed that he wouldn't have believed me completing it in only a few days because he's seen some applicants do about the same amount of work for the entire six week gap. In fact, Brian was so impressed with how my portfolio website turned out, that he had awarded me a scholarship.</p>
+                        <p>Working hard to absorb every bit of HTML, CSS, and JavaScript that I could, I finished all of the pre-course work in a few nights. I then passed the assessment test with an almost perfect score the next day. In the end, I was left with three days to build my portfolio from scratch.</p>
+                        
+                        <p>Brian hopped into a Zoom meeting with me on August 12th, 2020 for my Website Evaluation. He was quiet for the majority of the presentation and let me explain my code. At the end of the meeting, he asked if I had anything else to say about my website, and I confessed to him that I had only built it within the last three days.</p>
+                        
+                        <p>I had always kept Brian in the loop with my progress for the Springboard prep course. He didn't know it would take so much time away from me and was concerned that all of the assignments for General Assembly were done in one week.</p>
+                        
+                        <p>Despite my struggles, he told me that he was genuinely impressed with my portfolio website. He confessed that he wouldn't have believed me completing it in only a few days because he's seen some applicants do about the same amount of work for the entire six-week gap. In fact, Brian was so impressed with how my portfolio website turned out, that he awarded me a scholarship.</p>
                     </li>
 
                     <!-- Timeline Item -->
@@ -98,11 +98,11 @@
                         <div class="timeline-icon">5</div>
                         <h4>General Assembly</h4>
                         <div class="timeline-item-date">September 21, 2020</div>
-                        <p>Following my Website Evaluation and the granting of my scholarship, I was officially enrolled as a student in General Assembly's "Software Engineering Immersive (Remote)" course.</p>
-
-                        <p>My coding bootcamp cohort would take place from September 21st, 2020 - December 18th, 2020. Classes would be Monday - Friday from 9am - 5pm with a one hour lunch. Throughout the 12-week course, we would have 480 hours of scheduled class time. Factoring in homework and after-hour tutoring, there would be an overall count of 500+ hours.</p>
+                        <p>Following my Website Evaluation and being granted my scholarship, I was officially enrolled as a student in General Assembly's "Software Engineering Immersive (Remote)" course.</p>
                         
-                        <p>Students were expected to pass 80% of all assigned homework that would be due every Monday night. A mandatory of 5 school projects would have to be submitted, presented, and receive a passing grade in order for students to pass the entire program.</p>
+                        <p>My coding bootcamp cohort took place from September 21st, 2020 - December 18th, 2020. Classes were Monday - Friday from 9am - 5pm with a one-hour lunch. Throughout the 12-week course, we had 480 hours of scheduled class time. Factoring in homework and after-hour tutoring, there was an overall count of 500+ hours.</p>
+                        
+                        <p>Students were expected to pass 80% of all assigned homework that was due every Monday night. 5 mandatory school projects were to be submitted, presented, and receive a passing grade in order for students to pass the entire program.</p>
                     </li>
 
                     <!-- Timeline Item -->
@@ -110,11 +110,11 @@
                         <div class="timeline-icon">6</div>
                         <h4>Graduation</h4>
                         <div class="timeline-item-date">December 18th, 2020</div>
-                        <p>After the intense 3-month long program, it was finally graduation day. One-by-one, students who were able to meet all of the requirements had their named read off in the last zoom meeting of the cohort. Certificates of completion were soon emailed to students as proof of finishing the course.</p>
-
-                        <p>In the coding bootcamp I learned many things. On the technical skills side, I was able to learn about Node.js, Express.js, EJS, React, and PostgreSQL. Along with object-oriented programming, product development fundamentals, and team collaborating strategies.</p>
-
-                        <p>On a personal level, I was able to prove not only to my parents, but myself, that I was absolutely capable to following my own dreams and reaching my goals. I knew that if I wanted it, if I desired it, then I can achieve it.</p>
+                        <p>After the intense 3-month long program, it was finally graduation day. One-by-one, students who were able to meet all of the requirements had their names read off in the last Zoom meeting of the cohort. Certificates of completion were soon emailed to students as proof of finishing the course.</p>
+                        
+                        <p>In the coding bootcamp, I learned many things. On the technical skills side, I was able to learn about Node.js, Express.js, EJS, React, and PostgreSQL. Along with object-oriented programming, product development fundamentals, and team collaborating strategies.</p>
+                        
+                        <p>On a personal level, I was able to prove not only to my parents but to myself, that I was absolutely capable of following my own dreams and reaching my goals. I knew that if I wanted it, if I desired it, then I could achieve it.</p>
                     </li>
                 </ul>
                 <!-- end: Timeline -->
