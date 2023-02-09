5c541999c5d715bd4871f67cb5384401c9d2080c
Jeo <j.cabrigas.writer@gmail.com>
2023-02-09 00:11:13 -0800

    Adds more content to website portal

--- ajax/solano_computer_info.php
+++ ajax/solano_computer_info.php
@@ -9,11 +9,11 @@
 <div class="card-body">
     <div class="row">
         <div class="col-12 text-light">
-            <p>After acquisitioning this company, Tech 2U assigned me my 2nd internal rebuild project. The newly obtained company's last website was created in the early 2000's. It was very obviously outdated and was filled with pixelated pictures. It was time to put my frontend specialty to the test.</p>
+            <p>After acquisitioning this company, Tech 2U assigned me my 2nd internal rebuild project. The newly obtained company's last website was created in the early 2000's. It was filled with very obviously, outdated content and was home to some pixelated pictures. Some text even mentioned software that was no longer being used in the modern era. It was time to put my frontend specialty to the test.</p>
 
-            <p>Personally, I really like a monochrome look, so when I saw the original website was filled with black, white, and gray colors, I wanted to stick to it. Again I was told to practice on this internal website using new features. On one page I used text tiles that changed colors as a gradient. On another, I added a looping text animation to test the waters. The initial look was made sleeker and simpler while also having a lot of hover over features.</p>
+            <p>Personally, I really like a monochrome look, so when I saw the original website was filled with black, white, and gray colors, I wanted to stick to it. Again, I was told to practice on this internal website using new features that I've been eager to try out. On one page I used text tiles that changed colors as a gradient. On another, I wanted to test the waters by adding a looping text animation. The initial look was made sleeker and simpler while also having a lot of hover over features.</p>
 
-            <p>Upon submitting the monochrome website to my Senior Web Developer, he made the call to change the main color scheme to blue.</p>
+            <p>Upon submitting the monochrome website to my Senior Web Developer, he made the call to change the main color scheme to the current dark blue. He figured that a website that was mainly black and white would be too intimidating for visitors and would scare them away from doing business. He wanted a website that would house a color with a more "friendlier" tone.</p>
         </div>
     </div>
 
