4ac5bf6b28e43778ac8dec5a1d30de0b4c13b5e2
Jeo <j.cabrigas.writer@gmail.com>
2023-02-08 22:46:17 -0800

    Adds content to web portal

--- ajax/fireup_info.php
+++ ajax/fireup_info.php
@@ -9,7 +9,9 @@
 <div class="card-body">
     <div class="row">
         <div class="col-12 text-light">
-            <p>The CIO of Tech 2U has created another company called FireUp Esports Lounge. Although they already have a main website, I was tasked to help build the frontend of the reservation page. A booking system was created inside of a modal made from scratch/ It animates and highlights whenever a user makes a selection so they know what it is they are selecting.</p>
+            <p>The CIO of Tech 2U created another company called FireUp Esports Lounge. Although they already have a main website, I was tasked to help build the frontend of their room reservation page. A booking system was created to help customers reserve a party package at the lounge. They would be able to choose their desired room, select how long they want to reserve the room, and for what date they wanted to reserve the room on. Included in a user's party package was the room they reserved, a private cabana, a gaming hex pod, and bar discounts.</p>
+            
+            <p>The booking system was built inside of a custom portal which then processes payments using Square. As customers fill out the forms for their reservation, boxes were given small animations and highlights whenever a selection was made. There are other portals on the page that open up an FAQ section and explain what the private cabana and gaming hex pods are. The thought of a majority of customers booking through their cell phones came to mind, which then had the Senior Web Developer task me with making sure the reservation page was mobile-friendly.</p>
         </div>
     </div>
 
