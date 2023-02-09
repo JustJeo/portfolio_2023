33567e5615b2a146336e4c96501e4dff5fc0d4ed
Jeo <j.cabrigas.writer@gmail.com>
2023-02-08 23:56:28 -0800

    Adds more content to web portal

--- ajax/madden_lawyer_info.php
+++ ajax/madden_lawyer_info.php
@@ -9,7 +9,11 @@
 <div class="card-body">
     <div class="row">
         <div class="col-12 text-light">
-            <p>A client requested rush service for their brand new website. It was to debut at his company's grand opening event. My frontend co-worker and I had worked our butts off for the new few days to complete the website in time. We had implemented hand made "Read More" modals that would display in the middle of the website to learn the full story of each lawyer at the firm.</p>
+            <p>A client reached out and requested a rush service for their brand new website. It was to debut at his company's grand opening event on the first of the upcoming month. There was to be a home page, an about page to display all of the services and lawyers in the company, and a contact page. The client promised to provide us with all of their images, content, and contact information.</p>
+            
+            <p>My frontend co-worker and I were given a challenge to complete the entire website in 3 days, but the official deadline was in a week. We had worked our butts off for the next few days to complete the website's layout and to get ready to input all given content. We had even implemented "Read More" portals that were made from scratch. They would pop up in the middle of the page to display the history of each lawyer at the firm.</p>
+
+            <p>Despite being able to complete the website within the 3-day challenge time frame, we were unable to mark the project as complete until day four as we had to wait for the client to provide us the rest of the content. Regardless, the website was finished before the official deadline and made a grand debut at the company's opening event.</p>
         </div>
     </div>
 
