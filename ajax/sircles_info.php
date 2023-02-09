55dd84e3716bc57268cd9d559256bf208f040398
Jeo <j.cabrigas.writer@gmail.com>
2023-02-09 00:42:49 -0800

    Adds content to website synopsis

--- ajax/sircles_info.php
+++ ajax/sircles_info.php
@@ -9,13 +9,17 @@
 <div class="card-body">
     <div class="row">
         <div class="col-12 text-light">
-            <p>Sircles is the up and coming competitor for Yelp. It was designed to only recommend places and businesses if your friend "loved" it. No comments needed. Sircles was also created by the CEO of Tech 2U. They needed my help to work on the website's frontend. This was the first one-page project that I've worked on. There were many looping lottie videos and responsive design that had to be taken into account as Sircles is a mobile-first application.</p>
+            <p>Sircles is the up and coming competitor for Yelp. It was designed to only recommend places and businesses if your friends "loved" it. No comments needed. If your friends stopped "loving" a place, they simply removed the heart they once had on a business and their recommendation is removed from your list. Simple as that. Sircles was also created by the CEO of Tech 2U.</p>
+            
+            <p>The company needed my help to work on the website's frontend. This was the first one-page project that I've worked on since being employed. There were many looping lottie videos and responsive design that had to be taken into account as Sircles is a mobile-first application. Some animations were dependant on what content was showing on a user's screen, regardless of what device they were on.</p>
+
+            <p>This was also the first time that I had worked with another, already established, team. The Sircles team consisted of animators and graphic designers that provided all images and the lottie videos mentioned above. Much of their material came in layers and was tricky to work with since styling one layer usually affected another layer, or would completely render the whole graphic unusable.</p>
         </div>
     </div>
 
     <div class="row">
         <div class="col-12">
-            <p class="tech_used_portal"><span class="green_text bold underline">Technologies Used:</span> HTML, CSS, JavaScript, Bootstrap, PHP and AJAX.</p>
+            <p class="tech_used_portal"><span class="green_text bold underline">Technologies Used:</span> HTML, CSS, JavaScript, and Bootstrap</p>
         </div>
     </div>
 
