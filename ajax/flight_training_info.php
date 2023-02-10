41415d33341c9718dbbc1b084de6e83c6d16f3bd
Jeo <j.cabrigas.writer@gmail.com>
2023-02-09 23:08:40 -0800

    Adds final website synopsis

--- ajax/flight_training_info.php
+++ ajax/flight_training_info.php
@@ -9,7 +9,15 @@
 <div class="card-body">
     <div class="row">
         <div class="col-12 text-light">
-            <p>This client website was absolutely jam packed with content, but my frontend co-worker and I made sure to keep our heads above the clouds and follow the client's requests to the T. We had constant contact with the client and would utilize every bit of information and resource given to us. He had also given us much feedback and suggestions to improve the website to his liking in the final version.</p>
+            <p>Learn how to soar through the skies with Stealth Accelerated Flight Training. As the name suggests, this client is a certified pilot who teaches his students how to fly a plane swiftly within a month. We knew he was excited about his website as he almost immediately signed the paperwork for us to begin. There was only one problem: he had no official name for his company.</p>
+
+            <p>To ease his nerves, we told him that he could take his time as we built the skeletal structure of the website itself and that it would take a moment for us to begin inserting his information. Before we knew it, my frontend co-worker and I got started with the project. As I ported over libraries and set up the dev environment, my co-worker began the general layout of some pages. In the meantime, we were doing our best with the minimal information that we had.</p>
+
+            <p>Our Senior Web Developer told us that the client had a few names circling in his head; "Flying Tomatoes Private Pilot Program" and "Red Hot Flight Training" were just a few. Although there was no name set, my co-worker and I noticed the theming of the color red. We decided that we should have the website's colors be different shades of red and orange to match the reoccurring theme.</p>
+
+            <p>After a few days, the client responded to us with a document jam-packed with content. He also sent back some criticism of our rough version of the website. In short, he questioned why everything was red and orange. Instead, he requested to have the main colors of the website be blue and silver.</p>
+
+            <p>My frontend teammate and I made sure to keep our heads above the clouds and follow the client's requests to the T, despite having to go through 3 different iterations of the website and 4 name changes of the company. We utilized every bit of information and resource given to us to ensure that all content was accurate and up to date. Through constant feedback and suggestions to improve the website, we had come to the final version which satisfied his needs.</p>
         </div>
     </div>
 
