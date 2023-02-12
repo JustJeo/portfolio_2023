eb9723dc99a2f154514246aa606a25f56bedfaf0
Jeo <j.cabrigas.writer@gmail.com>
2023-02-11 16:44:20 -0800

    Styles mobile footer

--- sections/footer.php
+++ sections/footer.php
@@ -3,16 +3,16 @@
 	    <section class="p-b-15 p-t-15" id="footer-img">
 	        <div class="container">
 		        <div class="row align-items-center">
-					<div class="col-md-8">
+					<div class="col-lg-8 col-10">
                         <div class="row">
                             <!-- Email -->
-                            <div class="col-md-4 text-center footer_link" id="email-footer">
+                            <div class="col-md-4 col-12 text-center footer_link" id="email-footer">
                                 <h4><a href="mailto:j.cabrigas.writer@gmail.com" class="link_click underline">j.cabrigas.writer@gmail.com</a></h4>
                             </div>
                         </div>
                         <div class="row">
                             <!-- Github -->
-                            <div class="col-md-2 text-center" id="github-footer-col">
+                            <div class="col-md-2 col-6 text-center" id="github-footer-col">
                                 <div id="github-footer">
                                     <a href="https://github.com/JustJeo" target="_blank">
                                         <img src="assets/images/github-logo.png" id="github-logo-footer" class="footer_logo" alt="Black GitHub logo">
@@ -22,7 +22,7 @@
                             </div>
 
                             <!-- LinkedIn -->
-                            <div class="col-md-2 text-center">
+                            <div class="col-md-2 col-6 text-center">
                                 <div id="linkedin-footer">
                                     <a href="https://www.linkedin.com/in/j-cabrigas/" target="_blank">
                                         <img src="assets/images/linkedin-logo-black.png" id="linkedin-logo-footer" class="footer_logo" alt="Black LinkedIn logo">
@@ -36,7 +36,7 @@
                     <div class="col-md-4">
                         <div class="row">
                             <!-- Copyright -->
-                            <div class="col-md-7 text-center">
+                            <div class="col-md-7 col-10 text-center">
                                 <div class="copyright-text">Copyright &copy; <?php echo date("Y");?> Jamieden Cabrigas.<br>Once again I'm Jeo, Just Jeo.</div>
                             </div>
                         </div>
