2706d3261b39a809e6587163068f648f765c50ad
Jeo <j.cabrigas.writer@gmail.com>
2023-03-07 10:25:50 -0800

    Updates website preview title and description

--- sections/html_head.php
+++ sections/html_head.php
@@ -1,11 +1,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <meta name="author" content="Just Jeo" />    
-<meta name="description" content="I'm Jeo, Just Jeo, and you be you, just you.">
+<meta name="description" content="Frontend Web Developer Portfolio created by Jamie Cabrigas">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 <!-- Document title -->
-<title>Jeo's Portfolio</title>
+<title>Just Jamie's Portfolio</title>
 
 <!-- Stylesheets --> 
 <link href="assets/css/plugins.css" rel="stylesheet">
