9bd0938259f30de144b3ea671948bd0e106025c0
Jeo <j.cabrigas.writer@gmail.com>
2023-08-22 23:51:11 -0700

    Adds skeleton to portals of bus team and map training content

---
+++ ajax/business_team_info.php
@@ -0,0 +1,34 @@
+<div class="card-header">
+    <div class="row">
+        <div class="col-12">
+            <h3 class="modal_header">Tech 2U - Business Team Assist IT [2023]</h3>
+            <button type="button" class="btn btn-light btn-sm float-right" id="x-btn">X</button>
+        </div>
+    </div>
+</div>
+<div class="card-body">
+    <div class="row">
+        <div class="col-12 text-light">
+            <p></p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <p class="tech_used_portal"><span class="green_text bold underline">Technologies Used:</span> HTML, CSS, JavaScript, Bootstrap, PHP and AJAX.</p>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-12 text-center">
+            <a href="https://assist.tech2u.com/" target="_blank" class="btn btn-light visit_website_portal_btn">Visit Website</a>
+        </div>
+    </div>
+</div>
+
+<script>
+$("#x-btn").click(function() {
+    $("#project-portal").hide();
+});
+
+</script>
