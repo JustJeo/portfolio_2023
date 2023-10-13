839f9a888155b5481232252a1c2d2e636ecb6cba
Jeo <j.cabrigas.writer@gmail.com>
2023-10-13 15:22:03 -0700

    Adds content into blind shoppe info file

--- ajax/blind_shoppe_info.php
+++ ajax/blind_shoppe_info.php
@@ -9,7 +9,11 @@
 <div class="card-body">
     <div class="row">
         <div class="col-12 text-light">
-            <p></p>
+            <p>The Blind Shoppe is a blinds, shutters, and shades company that serves California's Central Valley area. Despite being established in 2008, the owners have never had a website that they were fully proud of. They reached out to the company for us to build a website after coming to terms with the fact that technology was definitely the way of the future and simply having flyers to pass out wasn't going to be enough anymore.</p>
+
+            <p>The business owners both admit that they were on the older side and were never really tech savvy to begin with. Despite this though, I made sure to be as descriptive, informative, and simple termed as possible. This was also the first time I was ever allowed to be as a main form of contact for a client who wasn't internal to the company. Admittedly, it was nerve wracking, sending emails and jumping onto calls, but as time went on I became more comfortable with contacting the clients.</p>
+
+            <p>In the end, the clients both shared their satisfaction with the website. They believed it looked very professional and were finally proud of a website to call their own. This website was also the first website where I had complete full reign with the design. The senior web developer had been so impressed with the frontend aspects for the past projects that I've worked on, that he gave me the green light to create what I wanted and how I wanted, of course, with the critique of the client. With many pictures, an image carousel, and a gallery page, it's one of the most simply artistic websites that I've created.</p>
         </div>
     </div>
 
