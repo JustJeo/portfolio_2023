b8ddffa36a62d8edac7679fb66b332bbbfa2aef2
Jeo <j.cabrigas.writer@gmail.com>
2023-10-13 14:52:25 -0700

    Adds The Blind Shoppe to list of Websites

---
+++ ajax/blind_shoppe_info.php
@@ -0,0 +1,28 @@
+<div class="card-header">
+    <div class="row">
+        <div class="col-12">
+            <h3 class="modal_header">The Blind Shoppe [2023]</h3>
+            <button type="button" class="btn btn-primary btn-sm float-right" id="x-btn">X</button>
+        </div>
+    </div>
+</div>
+<div class="card-body">
+    <div class="row">
+        <div class="col-12 text-light">
+            <p></p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <p class="tech_used_portal"><span class="green_text bold underline">Technologies Used:</span> HTML, CSS, JavaScript, Bootstrap, PHP, and AJAX.</p>
+        </div>
+    </div>
+</div>
+
+<script>
+$("#x-btn").click(function() {
+    $("#project-portal").hide();
+});
+
+</script>
