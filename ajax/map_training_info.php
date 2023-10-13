a965a29d00fbd8ebd20f19a8692fd2b6f73a69d8
Jeo <j.cabrigas.writer@gmail.com>
2023-10-13 15:46:46 -0700

    Adds content to map training info file

--- ajax/map_training_info.php
+++ ajax/map_training_info.php
@@ -9,13 +9,17 @@
 <div class="card-body">
     <div class="row">
         <div class="col-12 text-light">
-            <p></p>
+            <p>Get into shape with Mixed Athletic Performance Training, AKA MAP Training. This project was undoubtedly a lot of firsts for the company and the department. This would be our first gym client, our first time building software that was meant to replace 3rd party programs, and also the first project where we would be building a webstore! We, as a team, knew this was going to be a big project that was going to take a lot of time, but we also knew that we weren't going to back down.</p>
+
+            <p>Being the only frontend developer on the team, I knew that I had a lot on my plate as I was going to be building six public webpages. Five of those pages were going to be static redesigns of the client's old website and the last page was going to be a single page webstore, which the team had never built before. As the weeks turned into months, the giant pile of work began to dwindle. The client, whom the whole team was able to meet multiple times, was very cooperative and very insightful with what he wanted in his website. He was very quick to respond to questions [whether in person or digitally through emails] and informative of why he didn't want something a certain way.</p>
+
+            <p>This project showed a lot of the department coming together, handing off different parts to different people as we'd be done with a section. A lot of my interactions with the others was making a section of the webstore look pretty after the backend developers had got the logic to work. Other times, I would be collaborating with the senior web developer to clarify if something would be to his or the client's liking or not. In the end, it took many months and a lot of back and forth with the client, but it helped us get experience with many new things.</p>
         </div>
     </div>
 
     <div class="row">
         <div class="col-12">
-            <p class="tech_used_portal"><span class="green_text bold underline">Technologies Used:</span> HTML, CSS, JavaScript, Bootstrap, PHP and AJAX.</p>
+            <p class="tech_used_portal"><span class="green_text bold underline">Technologies Used:</span> HTML, CSS, JavaScript, Bootstrap, jQuery, PHP and AJAX.</p>
         </div>
     </div>
     
