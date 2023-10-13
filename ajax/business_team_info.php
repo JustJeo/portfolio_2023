2e8d558366195c84aa89d355ec252094ae2cb883
Jeo <j.cabrigas.writer@gmail.com>
2023-10-13 15:28:38 -0700

    Edits text in business team info file

--- ajax/business_team_info.php
+++ ajax/business_team_info.php
@@ -9,17 +9,17 @@
 <div class="card-body">
     <div class="row">
         <div class="col-12 text-light">
-            <p>Not long before this project was assigned to me, my other frontend co-worker had parted ways with the company and I was left to create static websites solo again. Thankfully, this project was an internal project and had no hard deadline attatched to it. The very popular business team with Tech 2U wanted a brand new website that they could share potential clients to hopefully seal the deal.</p>
+            <p>Not long before this project was assigned to me, my other frontend co-worker had parted ways with the company and I was left to create static websites solo again. Thankfully, this project was an internal project and had no hard deadline attatched to it. The very popular business team with Tech 2U wanted a brand new website that they could share with potential clients to hopefully seal the deal.</p>
 
             <p>The head of the business team dapartment was essentially the client, who passed off reference websites and kept in contact with the Senior Web Developer. The goal of the website was to be simple, yet informative, with a sense of uniformity. Since this website was a branch of Tech 2U, the General Manager wanted to oversee the project personally, wanting to make sure that it gave the same flow as the main Tech 2U website.</p>
 
-            <p>Working on this website, mainly solo, helped me refine some coding skills that I would normally pass on to my co-worker. It helped me fine tune my usual styling tactics and it also introduced me into making contact with the client. There were a few times throughout the project when I personally had to reach out to the head of the business team to make sure that the website was up to no only their standard, but the company's as well.</p>
+            <p>Working on this website, mainly solo, helped me refine some coding skills that I would normally pass on to my co-worker. It helped me fine tune my usual styling tactics and it also introduced me into making contact with the client. There were a few times throughout the project when I personally had to reach out to the head of the business team to make sure that the website was up to not only their standards, but the company's as well.</p>
         </div>
     </div>
 
     <div class="row">
         <div class="col-12">
-            <p class="tech_used_portal"><span class="green_text bold underline">Technologies Used:</span> HTML, CSS, JavaScript, Bootstrap, PHP and AJAX.</p>
+            <p class="tech_used_portal"><span class="green_text bold underline">Technologies Used:</span> HTML, CSS, JavaScript, Bootstrap, jQuery, PHP and AJAX.</p>
         </div>
     </div>
     
